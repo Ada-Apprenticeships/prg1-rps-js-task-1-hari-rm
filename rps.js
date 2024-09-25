85ed88c813aa18af05a15e95b3ccd0173faf353e
hari-rm <hari.richardsonmorja@ada.ac.uk>
2024-09-25 11:45:40 +0000

    Works for the 3 RPS options, outputs where player1, player2, or draw, no other features so far, passes test 1,2,3,4

--- rps.js
+++ rps.js
@@ -1,9 +1,30 @@
 function rockPaperScissors(player1, player2) {
 
+  if (player1 == player2) {
+    return("draw")
+  } 
+  if (player1 === "rock" && player2 === "scissors") {
+    return("player1")
+  } else if (player1 === "paper" && player2 === "rock") {
+    return("player1")
+  } else if (player1 === "scissors" && player2 === "paper") {
+    return("player1")
+  }
+ 
+  if (player2 === "rock" && player1 === "scissors") {
+    return("player2")
+  } else if (player2 === "paper" && player1 === "rock") {
+    return("player2")
+  } else if (player2 === "scissors" && player1 === "paper") {
+    return("player2")
+  }
+
+
   
    
 }
 
+console.log(rockPaperScissors("rock", "paper"))
 
 
 
