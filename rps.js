394399dfafedc02cb42860030920a60288d031ce
hari-rm <hari.richardsonmorja@ada.ac.uk>
2024-09-25 13:23:16 +0000

    Includes the RPS and the Big Bang version, passes all tests

--- rps.js
+++ rps.js
@@ -3,28 +3,33 @@ function rockPaperScissors(player1, player2) {
   if (player1 == player2) {
     return("draw")
   } 
-  if (player1 === "rock" && player2 === "scissors") {
+  if (player1 === "rock" && player2 === "scissors" || player1 === "rock" && player2 === "lizard") {
     return("player1")
-  } else if (player1 === "paper" && player2 === "rock") {
+  } else if (player1 === "paper" && player2 === "rock" || player1 === "paper" && player2 === "spock") {
     return("player1")
-  } else if (player1 === "scissors" && player2 === "paper") {
+  } else if (player1 === "scissors" && player2 === "paper" || player1 === "scissors" && player2 === "lizard") {
     return("player1")
-  }
+  } else if (player1 === "spock" && player2 === "scissors" || player1 === "spock" && player2 === "rock") {
+    return("player1")
+  } else if (player1 === "lizard" && player2 === "spock" || player1 === "lizard" && player2 === "paper") {
+    return("player1")
+  } 
  
-  if (player2 === "rock" && player1 === "scissors") {
+  if (player2 === "rock" && player1 === "scissors" || player2 === "rock" && player1 === "lizard") {
     return("player2")
-  } else if (player2 === "paper" && player1 === "rock") {
+  } else if (player2 === "paper" && player1 === "rock" || player2 === "paper" && player1 === "spock") {
     return("player2")
-  } else if (player2 === "scissors" && player1 === "paper") {
+  } else if (player2 === "scissors" && player1 === "paper" || player2 === "scissors" && player1 === "lizard") {
     return("player2")
-  }
+  } else if (player2 === "spock" && player1 === "scissors" || player2 === "spock" && player1 === "rock") {
+    return("player2")
+  } else if (player2 === "lizard" && player1 === "spock" || player2 === "lizard" && player1 === "paper") {
+    return("player2")
+  } 
 
-
-  
-   
 }
 
-console.log(rockPaperScissors("rock", "paper"))
+
 
 
 
