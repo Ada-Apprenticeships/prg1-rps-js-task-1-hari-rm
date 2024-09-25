aa1516b5bcfebb8525597ed835643f1c8f42a716
hari-rm <hari.richardsonmorja@ada.ac.uk>
2024-09-25 13:38:16 +0000

    Includes all combinations in a object array, then the conditional checks in the player object to see if they have the winning object

--- rps.js
+++ rps.js
@@ -1,41 +1,22 @@
 function rockPaperScissors(player1, player2) {
 
+  const winningCombinations = {
+    rock:["scissors", "lizard"],
+    paper:["rock", "spock"],
+    scissors:["paper", "lizard"],
+    spock:["scissors","rock"],
+    lizard:["spock","paper"]
+  }
+
+
   if (player1 == player2) {
     return("draw")
-  } 
-  if (player1 === "rock" && player2 === "scissors" || player1 === "rock" && player2 === "lizard") {
-    return("player1")
-  } else if (player1 === "paper" && player2 === "rock" || player1 === "paper" && player2 === "spock") {
-    return("player1")
-  } else if (player1 === "scissors" && player2 === "paper" || player1 === "scissors" && player2 === "lizard") {
-    return("player1")
-  } else if (player1 === "spock" && player2 === "scissors" || player1 === "spock" && player2 === "rock") {
-    return("player1")
-  } else if (player1 === "lizard" && player2 === "spock" || player1 === "lizard" && player2 === "paper") {
-    return("player1")
-  } 
- 
-  if (player2 === "rock" && player1 === "scissors" || player2 === "rock" && player1 === "lizard") {
-    return("player2")
-  } else if (player2 === "paper" && player1 === "rock" || player2 === "paper" && player1 === "spock") {
-    return("player2")
-  } else if (player2 === "scissors" && player1 === "paper" || player2 === "scissors" && player1 === "lizard") {
-    return("player2")
-  } else if (player2 === "spock" && player1 === "scissors" || player2 === "spock" && player1 === "rock") {
-    return("player2")
-  } else if (player2 === "lizard" && player1 === "spock" || player2 === "lizard" && player1 === "paper") {
-    return("player2")
-  } 
-
+  } else if (winningCombinations[player1].includes(player2)){
+    return ("player1")
+  } else if  (winningCombinations[player2].includes(player1)){
+    return ("player2")
+  }
 }
-
-
-
-
-
-
-
-
 
 
 
